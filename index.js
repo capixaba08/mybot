043a58012623c27161076b014d01f4b67aa38ad4
capixaba <capixaba@localhost.localdomain>
2017-06-02 19:59:03 -0300

    add http request

--- index.js
+++ index.js
@@ -55,6 +55,29 @@ function receivedMessage(event) {
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
+  var http = require('http');  
+
+  //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+  var options = {
+    host: 'www.random.org',
+    path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+  };
+
+  callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+    });
+  }
+
+  http.request(options, callback).end();
 
   console.log("Consulta base de dados SQL.... ID = " +senderID );
 
@@ -81,6 +104,8 @@ function receivedMessage(event) {
     });
   });
   */
+
+
   console.log("Received message for user %d and page %d at %d with message:", 
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
