91aca1b696e313436155890ce462895272c941e2
capixaba <capixaba@localhost.localdomain>
2017-06-02 22:24:02 -0300

    add http request v17

--- index.js
+++ index.js
@@ -60,14 +60,16 @@ function receivedMessage(event) {
   var jsonResponse;  
   var app = apiai("e095ccbab11b4a6297c0f6cb460f08a7");
 
+  var strResponse = "";
   var request = app.textRequest('ola', {
       sessionId: '7684522f-3e0c-49bf-b269-efd6ae3e4977'
   });
 
   request.on('response', function(response) {
       console.log("Inicio... ");
-      console.log(response.toString());
-      jsonResponse = JSON.parse(response);
+      console.log(response);
+      strResponse = response;
+      jsonResponse = JSON.parse(strResponse);
   });
 
   request.on('error', function(error) {
