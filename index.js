37c12005eaf50362677f2f709b3c0c5035b2c928
capixaba <capixaba@localhost.localdomain>
2017-06-02 22:39:01 -0300

    add http request v10

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function receivedMessage(event) {
   var app = apiai("e095ccbab11b4a6297c0f6cb460f08a7");
 
   var strResponse = "";
+  console.log("Event.message = " + event.message);
   var request = app.textRequest(event.message, {
       sessionId: senderID
   });
